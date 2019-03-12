eef5387568279841dbfc0ffaa4418b6b7c8b06fe
Vinay <vinay@outscal.com>
2019-03-12 11:56:24 +0530

    Base files added

---
+++ Assets/BowMaster_Project/Scripts/Multiplayer/MultiplayerService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Zenject;
+using UnityEngine;
+
+namespace UI
+{
+    public class MultiplayerService
+    {
+        public MultiplayerService()
+        {
+
+        }
+    }
+}
---
+++ Assets/BowMaster_Project/Scripts/Player/PlayerService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Zenject;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerService
+    {
+        public PlayerService()
+        {
+
+        }
+    }
+}

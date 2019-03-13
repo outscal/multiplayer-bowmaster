d57e567507c8e3bf9cd30f2289b8e1a0fbd516a4
Bhavyam Kr Goyal <bhavyam@outscal.com>
2019-03-13 11:04:16 +0530

    with ready to merge multiplayer

--- Assets/BowMaster_Project/Scripts/Multiplayer/PhotonMultiplayerScripts/CommunicationManager.cs
+++ Assets/BowMaster_Project/Scripts/Multiplayer/PhotonMultiplayerScripts/CommunicationManager.cs
@@ -6,30 +6,19 @@ using ExitGames.Client.Photon;
 
 namespace MultiplayerSystem
 {
-    public struct InputData
+    public class CommunicationManager : IOnEventCallback
     {
-        public Vector2 spos;
-        public string localPlayerID;
-        public int characterID;
-    }
-    public class CommunicationManager :MonoBehaviour, IOnEventCallback
-    {
-        [SerializeField] lplayer player;
-        InputData data = new InputData();
-        // Use this for initialization
-        private void OnEnable()
+        public CommunicationManager()
         {
             PhotonNetwork.AddCallbackTarget(this);
         }
-
-        // Update is called once per frame
-        void Update()
+        ~CommunicationManager()
         {
-
+            PhotonNetwork.RemoveCallbackTarget(this);
         }
-        public void setData(InputData dat)
+        public void setData(InputData data)
         {
-            data = dat;
+            
             data.localPlayerID = PhotonNetwork.LocalPlayer.UserId;
             Debug.Log("userId:" + PhotonNetwork.LocalPlayer.UserId);
             byte evCode = 1;
@@ -39,26 +28,16 @@ namespace MultiplayerSystem
             PhotonNetwork.RaiseEvent(evCode, content, raiseEventOptions, sendOptions);
             Debug.Log("Trying to send Event");
         }
-
-        public void spawn(Vector2 data)
-        {
-            Instantiate(player.gameObject, data, Quaternion.identity);
-        }
-
         public void OnEvent(EventData photonEvent)
         {
-
             Debug.Log("Event Recieved: " + photonEvent.Code);
             byte eventCode = photonEvent.Code;
-
             if (eventCode == 1)
             {
                 object[] data = (object[])photonEvent.CustomData;
                 Vector2 targetPosition = (Vector2)data[0];
                 Debug.Log("Spawn Recieved from: " + (string)data[1]);
-                spawn(targetPosition);
             }
-
         }
     }
 }

d73570b3f2fc476da49742173292f421905dba6d
Vinay <vinay@outscal.com>
2019-03-15 18:35:51 +0530

    basic camera system working

--- Assets/BowMaster_Project/Scripts/CameraSystem/CameraService.cs
+++ Assets/BowMaster_Project/Scripts/CameraSystem/CameraService.cs
@@ -33,9 +33,11 @@ namespace CameraSystem
                 turn2Pos = turn1Pos;
             }
             
+            mainCamera.orthographicSize = 7f;
+            iTween.MoveTo(mainCamera.gameObject, turn2Pos, 0.2f);
+            await new WaitForSeconds(0.2f);
             iTween.MoveTo(mainCamera.gameObject, turn1Pos, 0.2f);
             await new WaitForSeconds(0.2f);
-            mainCamera.orthographicSize = 7f;
         }
         async public void SwitchCamera()
         {
